16a280d9d1fe9c192e844b1dd9f333cbf767ee8d
devhub <devangpatel729@gmail.com>
2024-09-09 16:02:01 +0530

    Splitting Code Across Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       body: GradientContainer(),
     ),
   ));
 }
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )),
-      child: const Center(
-          child: Text(
-        "Dev",
-        style: TextStyle(color: Colors.white, fontSize: 28),
-      )),
-    );
-  }
-}
