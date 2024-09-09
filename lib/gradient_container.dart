2edc7e1f61ec0093d354097d2e4d21b2831ab503
devhub <devangpatel729@gmail.com>
2024-09-09 16:08:24 +0530

    Practice: Create a Custom Widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_feginners/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -7,18 +8,16 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 26, 2, 80),
-              Color.fromARGB(255, 45, 7, 98)
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          )),
+        colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98)
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
       child: const Center(
-          child: Text(
-            "Dev",
-            style: TextStyle(color: Colors.white, fontSize: 28),
-          )),
+        child: StyledText()
+      ),
     );
   }
 }
