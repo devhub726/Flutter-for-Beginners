8e5eabd97e852367ccba6f34aec597f41fbbc4c1
devhub <devangpatel729@gmail.com>
2024-09-09 15:56:24 +0530

    Building Custom Widgets

---
+++ android/app/src/main/kotlin/dev/com/flutter_for_feginners/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.com.flutter_for_feginners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: GradientContainer(),
+    ),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 45, 7, 98)
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: const Center(
+          child: Text(
+        "Dev",
+        style: TextStyle(color: Colors.white, fontSize: 28),
+      )),
+    );
+  }
+}
