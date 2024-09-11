e3e686a9721c7fb7cdcd27a6a7d604c7c2c65d7f
devhub <devangpatel729@gmail.com>
2024-09-11 15:08:22 +0530

    How NOT To Build Interactive Widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_for_feginners/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(
-      {super.key, required this.color1, required this.color2});
+  GradientContainer({super.key, required this.color1, required this.color2});
+
+  GradientContainer.deepPurple({super.key})
+      : color1 = const Color.fromARGB(255, 23, 5, 64),
+        color2 = const Color.fromARGB(255, 29, 4, 74);
 
   final Color color1;
   final Color color2;
-
-  void rollDice() {}
+  var activeDiceImage = "assets/images/dice/dice-1.png";
+  void rollDice() {
+    activeDiceImage = "assets/images/dice/dice-4.png";
+    print("Changing image...");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,17 +32,18 @@ class GradientContainer extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
-              "assets/images/dice/dice-1.png",
+              activeDiceImage,
               width: 201,
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             TextButton(
               onPressed: rollDice,
               style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28)
-              ),
+                  // padding: const EdgeInsets.only(top: 20),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
               child: const Text("Roll Dice"),
             ),
           ],
