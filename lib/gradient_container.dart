d83d45df3bda7f22e6cba85c70f5df60ff8b6c07
devhub <devangpatel729@gmail.com>
2024-09-11 15:58:04 +0530

    Introducing Stateful Widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_for_feginners/dice_rolles.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key, required this.color1, required this.color2});
+  const GradientContainer({super.key, required this.color1, required this.color2});
 
-  GradientContainer.deepPurple({super.key})
+  const GradientContainer.deepPurple({super.key})
       : color1 = const Color.fromARGB(255, 23, 5, 64),
         color2 = const Color.fromARGB(255, 29, 4, 74);
 
   final Color color1;
   final Color color2;
-  var activeDiceImage = "assets/images/dice/dice-1.png";
-  void rollDice() {
-    activeDiceImage = "assets/images/dice/dice-4.png";
-    print("Changing image...");
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -27,27 +24,8 @@ class GradientContainer extends StatelessWidget {
         begin: startAlignment,
         end: endAlignment,
       )),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 201,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  // padding: const EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28)),
-              child: const Text("Roll Dice"),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller()
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       body: GradientContainer(
-          color1: const Color.fromARGB(255, 23, 5, 64),
-          color2: const Color.fromARGB(255, 29, 4, 74)),
+          color1: Color.fromARGB(255, 23, 5, 64),
+          color2: Color.fromARGB(255, 29, 4, 74)),
     ),
   ));
 }
