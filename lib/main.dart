c1845c9718e71f011414ea01c40fbccacebb80f2
devhub <devangpatel729@gmail.com>
2024-09-16 22:58:42 +0530

    Handling User (Text) Input with the TextField Widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Expenses(),
+  runApp(const MaterialApp(
+    home: Expenses(),
   ));
 }
