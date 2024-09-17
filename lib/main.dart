a4870d0ad1c0215af2ac2a55d16e01d8a7bfaf98
devhub <devangpatel729@gmail.com>
2024-09-17 22:46:30 +0530

    Setting Text Themes

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,28 @@ var kColorScheme =
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
-        colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer)),
+      colorScheme: kColorScheme,
+      appBarTheme: AppBarTheme(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: CardTheme(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 14,
+            ),
+          ),
+    ),
     home: const Expenses(),
   ));
 }
