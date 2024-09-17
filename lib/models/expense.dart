8595711b2e0cfe5beb6114ee3a0a0836e32fcd8d
devhub <devangpatel729@gmail.com>
2024-09-17 23:57:35 +0530

    Adding Alternative Constructor Functions & Filtering Lists

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -40,6 +40,13 @@ class ExpenseBucket {
 
   const ExpenseBucket({required this.category, required this.expenses});
 
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where(
+              (expense) => expense.category == category,
+            )
+            .toList();
+
   double get totalExpense {
     double sum = 0;
 
