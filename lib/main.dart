a0e0125e130579e89597d930c3e46d377d89b9d7
devhub <devangpatel729@gmail.com>
2024-09-20 15:33:19 +0530

    Using a GridView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals_app/categories.dart';
 
 final theme = ThemeData(
     useMaterial3: true,
@@ -20,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: //Todo ...,
+      home: const CategoriesScreen(),
     );
   }
 }
